1206b26449a3bdfcfb8684f9e7ba70ff7871ffb4
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-11 23:56:28 -0400

    added new folder and files for next lession

---
+++ array_problems/index.js
@@ -0,0 +1,12 @@
+
+test1 = [1, 2, 3, 4]
+test2 = ["a", "b", "c"]
+
+function printReverse(array) {
+    array.forEach(number){
+        console.log(number)
+    };
+}
+
+printReverse(test1);
+printReverse(test2);
