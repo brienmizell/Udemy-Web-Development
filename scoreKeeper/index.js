ca218ffb8e5dabe801849d40a95c89b60c112768
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-16 17:39:16 -0400

    added scoreKeeper folder for making a scoreKeeper app

---
+++ scoreKeeper/index.js
@@ -0,0 +1,19 @@
+let p1Button = document.querySelector('#p1');
+let p2Button = document.getElementById('p2');
+var p1Display = document.querySelector('#p1Display');
+let p2Display = document.querySelector('#p2Display')
+let p1Score = 0;
+let p2Score = 0;
+
+console.log(p1Display);
+
+p1Button.addEventListener('click', function (){
+    p1Score++;
+
+    p1Display.textContent = p1Score
+})
+
+p2Button.addEventListener('click', function () {
+    p2Score++;
+    p2Display.textContent = p2Score;
+})
