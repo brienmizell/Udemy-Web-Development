05b483ae29992d06c4602169208f6d4a4b4032d5
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-16 17:56:06 -0400

    added to scoreKeeper so that when a player gets to 5 points no player can add points.

--- scoreKeeper/index.js
+++ scoreKeeper/index.js
@@ -1,19 +1,31 @@
 let p1Button = document.querySelector('#p1');
 let p2Button = document.getElementById('p2');
-var p1Display = document.querySelector('#p1Display');
+let p1Display = document.querySelector('#p1Display');
 let p2Display = document.querySelector('#p2Display')
 let p1Score = 0;
 let p2Score = 0;
+let gameover = false;
+let winningScore = 5;
 
 console.log(p1Display);
 
 p1Button.addEventListener('click', function (){
-    p1Score++;
-
-    p1Display.textContent = p1Score
-})
+    if(!gameover) {
+        p1Score++;
+        if(p1Score === winningScore) {
+            gameover = true;
+        }
+        p1Display.textContent = p1Score
+    }
+    
+});
 
 p2Button.addEventListener('click', function () {
-    p2Score++;
-    p2Display.textContent = p2Score;
+    if(!gameover) {
+        p2Score++;
+        if (p2Score === winningScore) {
+            gameover = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 })
