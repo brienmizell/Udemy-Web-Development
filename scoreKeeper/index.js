34228e9552c71c236511f83529a3f566ab05bebe
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-16 21:49:12 -0400

    added functionality to reset button

--- scoreKeeper/index.js
+++ scoreKeeper/index.js
@@ -1,20 +1,20 @@
 let p1Button = document.querySelector('#p1');
 let p2Button = document.getElementById('p2');
+var resetButton = document.getElementById('reset');
 let p1Display = document.querySelector('#p1Display');
 let p2Display = document.querySelector('#p2Display')
 let p1Score = 0;
 let p2Score = 0;
-let gameover = false;
+let gameOver = false;
 let winningScore = 5;
 
-console.log(p1Display);
 
 p1Button.addEventListener('click', function (){
-    if(!gameover) {
+    if(!gameOver) {
         p1Score++;
         if(p1Score === winningScore) {
             p1Display.classList.add('winner');
-            gameover = true;
+            gameOver = true;
         }
         p1Display.textContent = p1Score
     }
@@ -22,12 +22,22 @@ p1Button.addEventListener('click', function (){
 });
 
 p2Button.addEventListener('click', function () {
-    if(!gameover) {
+    if(!gameOver) {
         p2Score++;
         if (p2Score === winningScore) {
             p2Display.classList.add('winner');
-            gameover = true;
+            gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
+})
+
+resetButton.addEventListener('click', function() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner');
+    p2Display.classList.remove('winner');
+    gameOver = false;
 })
