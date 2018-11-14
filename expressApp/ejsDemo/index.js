2b88eae48cd162329e7f1049c9b7be3e45fb15fe
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-11-13 21:48:21 -0500

    included public assets

--- expressApp/ejsDemo/index.js
+++ expressApp/ejsDemo/index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.render('home.ejs');
+	res.render('home');
 	res.send('Welcome to the homepage');
 });
 
 app.get('/fallinlovewith/:thing', function(req, res) {
 	var thing = req.params.thing;
 	// res.send('You fell in love with ' + thing);
-	res.render('love.ejs', { thingVar: thing });
+	res.render('love', { thingVar: thing });
 });
 
 app.get('/posts', function(req, res) {
@@ -18,9 +21,9 @@ app.get('/posts', function(req, res) {
 		{ title: 'My adorable pet bunny.', author: 'Charlie' },
 		{ title: 'Can you believe this pomsky?', author: 'Colt' }
 	];
-	res.render('posts.ejs', { posts: posts });
+	res.render('posts', { posts: posts });
 });
 
-app.listen(3000, process.env.IP, function() {
+app.listen(3000, function() {
 	console.log('The server has started!');
 });
